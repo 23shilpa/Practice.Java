3b695eb58ad45f2e849f571b4d7d205b4506df9a
shilpa <shilpabhonde000@gmail.com>
2023-08-28 12:30:17 +0530

    first commit

---
+++ PracticeProblem/src/com/bridgelabz/IfElseIfExample.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+public class IfElseIfExample {
+    public static void main(String[] args) {
+        int marks = 50;
+        if(marks<50){
+            System.out.println("fail");
+        }
+        else if(marks>40){
+            System.out.println("pass");
+        }
+        else{
+            System.out.println("Invalid");
+        }
+    }
+}
---
+++ PracticeProblem/src/com/bridgelabz/IfExample.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class IfExample {
+    public static void main(String[] args) {
+        int Age = 30;
+        if(Age>20){
+            System.out.println("Age is greater than 20");
+        }
+    }
+}
